14d6ede1db895129af26e5a76f32ab3ea0b8ebf0
kkmike999@163.com <kkmike999@163.com>
2017-11-10 11:17:31 +0800

    优化获取build-tools/dx逻辑

--- rpc/src/main/java/com/dex/rpc/tools/Dx.java
+++ rpc/src/main/java/com/dex/rpc/tools/Dx.java
@@ -1,6 +1,11 @@
 package com.dex.rpc.tools;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,12 +41,13 @@ public class Dx {
     public String dx(String jarPath, String dexName) {
         String parent = new File(jarPath).getParent();
 
-        // "$ANDROID_HOME/build-tools/26.0.2/dx --dex --output=dex.jar myjar.jar"
-        File   dex = new File(parent, dexName);
-        File   jar = new File(jarPath);
-        String dx  = ANDROID_HOME + "/build-tools/26.0.2/dx --dex --output=" + dexName + " " + jar.getName();
+        // 例子，"$ANDROID_HOME/build-tools/27.0.1/dx --dex --output=dex.jar myjar.jar"
+        File dex = new File(parent, dexName);
+        File jar = new File(jarPath);
 
-        File dxFile = new File(ANDROID_HOME + "/build-tools/26.0.2/dx");
+        String buildToolsVersion = getLastBuildToolsVersion();
+        File   dxFile            = new File(ANDROID_HOME + "/build-tools/" + buildToolsVersion + "/dx");
+        String dx                = dxFile.getPath() + " --dex --output=" + dexName + " " + jar.getName();
 
         if (!dxFile.exists()) {
             throw new RuntimeException(dxFile.getPath() + " 不存在");
@@ -58,6 +64,40 @@ public class Dx {
     private void checkEnv(Map<String, String> envMap, String key) {
         if (!envMap.containsKey(key)) {
             throw new RuntimeException("没有$" + key + "环境变量");
+        }
+    }
+
+    /**
+     * 获取sdk build-tools 最新版本
+     *
+     * @return
+     */
+    private String getLastBuildToolsVersion() {
+        File buildToolFile = new File(ANDROID_HOME + "/build-tools/");
+
+        if (!buildToolFile.exists()) {
+            throw new RuntimeException(buildToolFile.getPath() + " not exist.");
+        }
+
+        List<String> names = new ArrayList<>(Arrays.asList(buildToolFile.list()));
+        names.remove(".DS_Store");
+
+        if (names.size() > 0) {
+            // 按build tools version 大到小排序
+            Collections.sort(names, new Comparator<String>() {
+                @Override
+                public int compare(String name1, String name2) {
+                    name1 = name1.replace(".", "");
+                    name2 = name2.replace(".", "");
+                    return -(Integer.valueOf(name1) - Integer.valueOf(name2));
+                }
+            });
+
+            String buildToolsVersion = names.get(0);
+
+            return buildToolsVersion;
+        } else {
+            throw new RuntimeException(buildToolFile.getPath() + " has no build tool");
         }
     }
 }
